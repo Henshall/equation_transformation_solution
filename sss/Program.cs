92850f69ad491423a97db50b03f8fea8a1362a25
wes <weshenshall@gmail.com>
2017-01-19 20:27:41 -0800

    reworked math method

--- sss/Program.cs
+++ sss/Program.cs
@@ -12,7 +12,9 @@ namespace sss
 
 
 		/// define the equation
-		public static string equation = "x^2 + 3.5xy + y = y^2 - xy + y";
+		/// public static string equation = "x^2 + 3.5xy + y = y^2 - xy + y";
+		public static string equation = "- x^2 + 3.5xy + y = y^2 - xy - y - y - y - y - y - y";
+
 
 		///  Create an equation list and parses the equation by spaces and puts each element in the list
 		/// 
@@ -119,8 +121,8 @@ namespace sss
 		public void add_equals_and_zero_to_end()
 
 		{
-			equation_list_to_ones.Add("=");
-			equation_list_to_ones.Add( "0");
+			concat_list.Add("=");
+			concat_list.Add( "0");
 
 		}
 
@@ -174,7 +176,6 @@ namespace sss
 		public static List<string> equal_list = new List<string>();
 
 		public static List<string> equal_table = new List<string>();
-		int i = 0;
 		int j = 0;
 		bool found = false;
 
@@ -265,12 +266,26 @@ namespace sss
 		}
 
 		public static List<string> addition_list = new List<string>();
+		public static List<string> count_list = new List<string>();
+
 
 
 		public void perform_math()
 		{
 
+			//int i = 0;
+			//foreach (string element in concat_list)
+			//{
+			//	Console.WriteLine(concat_list[i]);
+			//	i = i + 1;
+
+			//}
+		
+
+
 			int i = 0;
+
+
 			foreach (string element in equal_list)
 			{
 						string input = concat_list[i];
@@ -283,43 +298,72 @@ namespace sss
 			}
 
 			i = 0;
-			foreach (string element2 in equal_list)
+			int j = 0;
+
+			concat_list.Add(" ");
+
+			while (i < equal_list.Count - count_list.Count)
 			{
-				if (i == 0)
-				{
-				}
-				else
-				{
-					if (equal_list[i] == equal_list[i - 1])
+				
+				if (equal_list[i] == equal_list[i + 1] && equal_list[i] != "_")
 					{
-						string number_one = Regex.Replace(addition_list[i - 1], @"[+-]", "");
-						string number_two = Regex.Replace(addition_list[i], @"[+-]", "");
-						string sign_number_one = Regex.Replace(addition_list[i - 1], @"[^+-]", "");
-						string sign_number_two = Regex.Replace(addition_list[i], @"[^+-]", "");
+						string number_one = Regex.Replace(addition_list[i], @"[+-]", "");
+						string number_two = Regex.Replace(addition_list[i + 1], @"[+-]", "");
+						string sign_number_one = Regex.Replace(addition_list[i], @"[^+-]", "");
+						string sign_number_two = Regex.Replace(addition_list[i + 1], @"[^+-]", "");
 
 						float sum = float.Parse(number_one) + float.Parse(number_two);
 						float subtract = float.Parse(number_one) - float.Parse(number_two);
 						string sum_from_int = sum.ToString() + equal_list[i];
 						string subtract_from_int = subtract.ToString() + equal_list[i];
 
-					
+					Console.WriteLine(number_one);
+					Console.WriteLine(number_two);
+
+
+					concat_list.Add("_");
+					concat_list.RemoveAt(i);
+					concat_list.RemoveAt(i);
+
+
+
+					addition_list.RemoveAt(i);
+					addition_list.Insert(i, "+" + sum_from_int);
+
+
+					equal_list.RemoveAt(i + 1);
+					equal_list.Add("-");
+					j = j + 1;
+
 
 						if (sign_number_one == sign_number_two)
+					{
+						char[] element_array = sum_from_int.ToArray();
+						/// Console.WriteLine(element_array[0]);
+						string first_char = element_array[0].ToString();
+						if (first_char == "-" && first_char != "0")
 						{
-							concat_list.RemoveAt(i);
-							concat_list.Insert(i, sum_from_int);
-							concat_list.RemoveAt(i - 1);
 						}
-						else if (sign_number_one == "+")
+						else
 						{
-							concat_list.RemoveAt(i);
-							concat_list.Insert(i, subtract_from_int);
-							concat_list.RemoveAt(i-1);
+							concat_list.Insert(i, "+" + sum_from_int);
 						}
+
+						///Console.WriteLine(sign_number_one);
 					}
-				}
+					else if (sign_number_one == "+")
+					{
+						concat_list.Insert(i, subtract_from_int);
+ 					}
+					}
+
+
+
 				i = i + 1;
-			}
+
+				}  /// while loop ending
+				
+
 		}
 
 
@@ -339,9 +383,8 @@ namespace sss
 				if (first_char == "0")
 				{
 					concat_list.RemoveAt(i);
-					Console.WriteLine("removed!");
 				}
-				else if (first_char != "+" && first_char != "+")
+				else if (first_char != "+" && first_char != "-")
 				{
 					concat_list.Insert(i, "+" + concat_list[i]);
 					concat_list.RemoveAt(i + 1);
@@ -369,7 +412,9 @@ namespace sss
 		{
 
 			string joined = string.Join(" ", concat_list.ToArray());
+			Console.WriteLine("-------------");
 			Console.WriteLine(joined);
+			Console.WriteLine("-------------");
 
 		}
 
@@ -422,221 +467,20 @@ namespace sss
 			m.to_concat_list();
 			m.create_equals_table();
 			m.perform_math();
-			m.add_signs_and_remove_0s();
+		//	m.add_signs_and_remove_0s();
 			m.add_equals_and_zero_to_end();
 			m.join_to_string();
-
-
-			//	Array.Sort(concat_array);
-			// m.sort_array();
-			///equation_list_to_ones.Sort();
-
-			foreach (string element in equation_list_to_ones)
-
-			{
-				///Console.WriteLine(element);
-			}
-
-
-
-
-
-
-
-
 
 
 		
 
 
-
-
-				/// create a reference list which groups like terms
-
-			//	List<int> reference_list = new List<int>();
-			//	 i = 1;
-			//foreach (string element in equal_table)
-			//{
-
-			//	 j = 1;
-			//	foreach (string subelement in equal_table)
-			//	{
-			//		if (j == i)
-			//		{
-
-			//		}
-			//		else if (subelement == element)
-						
-			//		{
-			//			reference_list.Add(j);
-			//			break;
-			//		}
-			//		else if(j == 6)
-
-			//		{
-						
-			//			reference_list.Add(0);
-			//		}
-			//						j = j + 1;
-			//	}
-			//	i = i + 1;
-			//}
-
-			/////   Add like terms 		 
-			///// grabs second non-operator string in equation list
-
-			//i = 0;  /// reference list number
-			//j = 0;  /// equation list number (includs all values)
-			//int k = 0;  /// equation list number (excludes operators)
-			//int count2 = 0;
-			///// element = value in reference list
-			///// subelement = value in equation list
-			///// 
-			///// 
-			//List<int> addition_list = new List<int>();  //// creates an addition list where the numbers will be added
-			//List<int> sum_list = new List<int>();  //// creates an sum list where our values will be sumed
-
-
-			//foreach (int element in reference_list)
-
-			//{
-			//	if (element == 0)
-			//	{
-			//	}
-			//	else
-			//	{
-					
-			//		/// uses reference list position and values to addition_list. We will be able to use this list in the next step to perform the algebra
-			//		int ref_list_position = i;
-			//		int ref_list_number = reference_list[i];
-			//		addition_list.Add(ref_list_position);
-			//		string element2 = element.ToString();
-			//		string[] element3 = element2.Split();     
-
-			//		foreach (string character in element3)
-			//		{
-			//			int element4 = int.Parse(character);
-			//			addition_list.Add(element4);
-			//		}
-
-
-
-			//		/// here we take the numbers from the equation_list and put them in the sum_list
-
-			//		foreach (int add_value in addition_list)
-
-			//		{
-
-			//			// Get the numbers and put them in a sum_table
-			//			string whole_value = equation_list_to_ones[add_value];
-
-			//			string[] whole_value_array = whole_value.Split();
-			//			i = 0;
-			//			 count2 = 0;
-			//			foreach (string whole_value_element in whole_value_array)
-			//			{
-			//				if (whole_value_element == "a" || whole_value_element == "b" || whole_value_element == "c" || whole_value_element == "d" || whole_value_element == "e" || whole_value_element == "f" || whole_value_element == "g" || whole_value_element == "h" || whole_value_element == "i" || whole_value_element == "j" || whole_value_element == "k" || whole_value_element == "l" || whole_value_element == "m" || whole_value_element == "n" || whole_value_element == "o" || whole_value_element == "p" || whole_value_element == "q" || whole_value_element == "r" || whole_value_element == "s" || whole_value_element == "t" || whole_value_element == "u" || whole_value_element == "v" || whole_value_element == "w" || whole_value_element == "x" || whole_value_element == "y" || whole_value_element == "z")
-			//				{
-			//					count2 = i;
-			//					break;
-			//				}
-
-			//				i = i + 1;
-			//			}
-
-			//			/// make amalgomation list to grab the correct values out of the whole values array
-			//			i = 0;
-			//			List<string> amalgomation_list = new List<string>();
-			//			foreach (string string_element in whole_value_array)
-			//			{
-			//				if (i <= count2)
-			//				{
-			//					amalgomation_list.Add(string_element);
-			//				}
-			//				i = i + 1;
-			//			}
-
-
-						/// join the amalgomation list into one variable and add it to sum_list
-				///		string amalgomation = string.Join("", amalgomation_list.ToArray());
-
-					///	int amalgomation_int = int.Parse(amalgomation);
-
-					///	sum_list.Add(amalgomation_int);
-					
-				///	}
-
-				
-
-					/// 
-					//foreach (int addition in sum_list)
-					//{
-
-					//}
-
-
-
-
-
-					//foreach (string subelement in equation_list)
-					//{
-					//	if (subelement == "-" || subelement == "+")
-					//	{
-					//		///Console.WriteLine(subelement);
-
-					//	}
-					//	else
-					//	{
-					//		if (k == i)
-					//		{
-
-								/// subelement = 3.5xy
-								/// element = 5
-
-								///string value_one = subelement;
-								///string value_two = equation_list[element];
-
-						 ///    Console.WriteLine(subelement);
-							///	Console.WriteLine(value_two);
-			//				}
-			//					k = k + 1;
-			//			}
-			//			j = j + 1;
-			//		}
-
-
-
-
-			//	}
-			//	i = i + 1;
-			//}
-
-
-
-
-
-
-		///	PrintKeysAndValues(myKeys, myValues);
-
-
-
-
-
-
 			foreach (string element in concat_list)
 			{
-		//	Console.WriteLine(element);
+			//Console.WriteLine(element);
 
 			}
 
-
-			//foreach (int element in reference_list)
-			//{
-			//Console.WriteLine(element);
-
-			//}
-			/// why cant I override variables?
-				
 		}
 	}
 }
